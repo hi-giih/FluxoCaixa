81088667272a0edc30b8ecdfa1e0eef688235d86
Giovanna <gihsantos494@gmail.com>
2024-06-03 23:07:07 -0300

    Tela Cadastro

---
+++ src/componentes/bannerLatera.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import img_logo from '../assets/logo_cad.png'
+
+export default function BannerLateral(){
+    return(
+        <>
+        <div className="logo">
+            <img src={img_logo} alt="Logo" className="img_logo" width="55%"></img>
+            <p className='p_logo'> Abacaxi Topzeira</p>
+      </div>
+
+      <style>{`
+        .logo{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            width:50%;
+            background-color: #FFD0D0;
+          }
+        .p_logo{
+            background-color: #3AA6B9;
+            padding: 1% 6%;
+            width: 50%;
+            text-align: center;
+          }
+
+          `}
+      </style>
+        </>
+    )
+}
---
+++ src/componentes/input.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Input(){
+return(
+    <>
+    <input className="input"></input>
+    
+    <style>{`
+        .input{
+            background-color: #3AA6B9;
+            border-color: #3AA6B9;
+    
+        }
+        `}
+    </style>
+    </>
+);
+}
